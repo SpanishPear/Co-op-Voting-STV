3c44fb480e10c6c02351c410868bb362d4e2e449
shrey <z5257343@unsw.edu.au>
2020-08-12 21:33:29 +1000

    intial roles

---
+++ roles.py
@@ -0,0 +1,6 @@
+'''
+All roles that the previous executive team wish to hold an election for. 
+Roles should be EXACTLY that used in the google form question
+'''
+
+roles = ["pres", "vice-pres", "secretary", "arc-delegate", "treasurer", "social", "marketing", "charity", "IT", "publications"]
